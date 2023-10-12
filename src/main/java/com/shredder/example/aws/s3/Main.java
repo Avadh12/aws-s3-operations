9203280c61c07f2e27721fc6e3cd08bca0a7e073
avadhsharma010 <147719208+avadhsharma010@users.noreply.github.com>
2023-10-12 21:26:27 +0530

    Update Main.java

--- src/main/java/com/shredder/example/aws/s3/Main.java
+++ src/main/java/com/shredder/example/aws/s3/Main.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
+        teacher.setId(id);
+		teacher.setName(name);
+		teacher.setEmail(email);
+		teacher.setPhone(phone);
 
     }
 }
